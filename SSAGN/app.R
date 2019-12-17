88db433f5a7f52cad08df7088b39a2731986ab63
Nathan Pollesch <pollesch.nathan@epa.gov>
2019-12-17 10:21:50 -0600

    First version of SSAGAN Shiny
    Built first version of Shiny app, added details and panel view, hover
    and zoom functions.

--- SSAGN/app.R
+++ SSAGN/app.R
@@ -9,34 +9,117 @@
 
 library(shiny)
 library(visNetwork)
+library(googledrive)
+library(plyr)
+library(shinythemes)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-visNetworkOutput("network_hello",height = "800px")
+# Define UI for application that draws network
+ui <- fluidPage( theme = shinytheme("cyborg"),
+    
+    titlePanel("Superior Science Advocacy Group/Network"),
+    #setBackgroundColor(color="Black"),
+      sidebarLayout(
+      
+      sidebarPanel(
+        uiOutput("choose_node"),
+        actionButton("focus_now", "Zoom in!"),
+        uiOutput("choose_node_inst"),
+        actionButton("focus_now_inst", "Zoom in!"),
+        actionButton("zoom_out","Zoom out!")
+        ),
+      
+      mainPanel(
+        h4(textOutput("shiny_return")),
+        visNetworkOutput("network_proxy_nodes",height = "800px")
+        #h4(textOutput("nodes_data_from_shiny"))
+      )
+    )
+#visNetworkOutput("network_hello",height = "800px")
     
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw network
 server <- function(input, output) {
+
+  network_raw<-read.csv("cstpnetwork.csv")
+  nodes <- data.frame(id = network_raw$Name, Affiliation1 = network_raw$Affiliation.1, Affiliation2 = network_raw$Affiliation.2, CSTPEventTitle=network_raw$Presentation.Title, person=T)
+  nodes<-rbind.fill(nodes,data.frame(id = unique(network_raw$Affiliation.1), person=F))
+  nodes<-rbind.fill(nodes,data.frame(id = unique(network_raw$Affiliation.2), person=F))
+  nodes<-rbind.fill(nodes,data.frame(id = "Cafe Scientifique Twin Ports", person=F,image="iconcstp.jpg",shape="circularImage"))
+  nodes$Affiliation2[which(nodes$Affiliation2=="")]<-NA
+  nodes<-nodes[!duplicated(nodes$id),]
+  nodes<-nodes[-which(nodes$id==""),]
+  nodes<-cbind(nodes,font.color="White",font.size=20)
+  
+  CSTPtoPeeps<-unique(data.frame(from="Cafe Scientifique Twin Ports",to=nodes$id[nodes$person]))
+
+  Af1toAf2<-unique(data.frame(from=nodes[which(!is.na(nodes$Affiliation2)),2],to=nodes[which(!is.na(nodes$Affiliation2)),3]))
+  PeepstoAf2<-unique(data.frame(from=nodes$id[nodes$person],to=nodes$Affiliation2[nodes$person]))
+  PeepstoAf2<-PeepstoAf2[!is.na(PeepstoAf2[,2]),]
+  PeepstoAf1<-unique(data.frame(from=nodes$id[nodes$person],to=nodes$Affiliation1[nodes$person]))
+  
+  edges<-rbind(CSTPtoPeeps,Af1toAf2,PeepstoAf2,PeepstoAf1)
+  
+  output$network_proxy_nodes <- renderVisNetwork({
+    visNetwork(nodes, edges) %>%
+      visInteraction(hover = T) %>%
+      visEvents(hoverNode = "function(nodes) {
+        Shiny.onInputChange('current_node_id', nodes);
+      ;}") %>%
+visEvents(select = "function(nodes){Shiny.onInputChange('current_node_id',nodes.nodes);
+                ;}")
+  })
+  
+  
+  output$shiny_return <-
+  renderText({
+    if(is.null(input$current_node_id)){"Hover for info"}
+    else(input$current_node_id[[1]])
+  })
+  
+  myNode <- reactiveValues(selected = '')
+  
+  observeEvent(input$current_node_id, {
+    myNode$selected <<- input$current_node_id
+  })
+  
+  output$table <- renderText({
+    nodes[which(myNode$selected == nodes$id),]
+  })
+  
+  
+  
+  
+  
+  observeEvent(input$focus_now, {
+    visNetworkProxy("network_proxy_nodes") %>%
+      visFocus(id = input$Focus, scale = 2)
+      
+  })
+  
+  observeEvent(input$focus_now_inst, {
+    visNetworkProxy("network_proxy_nodes") %>%
+      visFocus(id = input$Focus_inst, scale = 2)
     
-    output$network_hello<-renderVisNetwork({
+  })
+  
+  observeEvent(input$zoom_out, {
+    visNetworkProxy("network_proxy_nodes") %>%
+      visFit()
     
-    nodes <- data.frame(id = 1:3, group = c("B", "A", "B"))
-    edges <- data.frame(from = c(1,2), to = c(2,3))
+  })
+  
+  output$choose_node <- renderUI({
+    selectInput("Focus", "Find a person:",
+                nodes$id[which(nodes$person==T)])
+  })
+  
+  output$choose_node_inst <- renderUI({
+    selectInput("Focus_inst", "Find an institution:",
+                nodes$id[which(nodes$person==F)])
+  })
     
-    visNetwork(nodes, edges, main="Nate is learning Shiny", submain="It is going ok") %>%
-        visGroups(groupname = "A", shape = "icon", icon = list(code = "f0c0", size = 75)) %>%
-        visGroups(groupname = "B", shape = "icon", icon = list(code = "f007", color = "red")) %>%
-        addFontAwesome() %>%
-        visLegend(addNodes = data.frame(label = c("A", "B"), shape = "icon", 
-                                        icon.code = c("f0c0", "f007"), 
-                                        icon.size = c(25,50), 
-                                        icon.color = c(NA, "red")),
-                  addEdges = data.frame(label = "link"), useGroups = FALSE)
-})
-    
-    }
+  }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
